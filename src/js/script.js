f301feb5f0c949ae3653b3a6395d9b88df269450
gatefait-mathilde <mathilde.gatefait@edu.univ-fcomte.fr>
2025-01-04 19:03:50 +0100

    css footer, menu, page1

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,16 @@
 
 //menu// 
+
+const toggle = document.querySelector(".menu-btn");
+const nav = document.querySelector(".headermenu");
+const page = document.body;
+
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    toggle.ariaExpanded = isClosed;
+    nav.ariaHidden = isOpen;
+    page.classList.toggle("noscroll", isClosed);
+  });
+}
